fc89be1d5dec29b9e11cab08ff78386a0c59d0c2
okuisatoshi <okuisatoshi@mac.com>
2020-10-13 18:45:35 +0900

    first commit

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
 })
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+bluetooth.startAccelerometerService()
+bluetooth.startTemperatureService()
+bluetooth.startMagnetometerService()
+bluetooth.startButtonService()
+bluetooth.startLEDService()
